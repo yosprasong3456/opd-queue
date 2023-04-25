04ccd5e8c80ee1f80d6888ab4ee9c634c764d9b5
yosprasong3456 <yosprasong3456@gmail.com>
2023-04-25 10:01:16 +0700

    env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 //test_site xampp db
-export const apiUrl = process.env.NODE_ENV === "development" ? process.env.REACT_APP_API_TESTSITE : process.env.REACT_APP_API_URL;
+export const apiUrl = process.env.NODE_ENV === "development" ? 'http://localhost/opd_queue_api/' : process.env.REACT_APP_API_URL;
 //production
 // export const apiUrl = "https://opd-queue.udch.work/api/";
 
